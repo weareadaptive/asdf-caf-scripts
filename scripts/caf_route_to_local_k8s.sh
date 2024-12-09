27d716febe2beb2a88d717070e1c4df9724a1d6c
Jason Garcia <jason.garcia.010@gmail.com>
2024-12-09 13:34:02 +0800

    Testing script changes to sudo

--- scripts/caf_route_to_local_k8s.sh
+++ scripts/caf_route_to_local_k8s.sh
@@ -195,4 +195,4 @@ fi
 
 # We have to define to functions we've declared inside the sudo session dynamically
 # As we must have the running sudo session active for the cleanup to be guarenteed to work
-sudo -E bash -c "$(declare -f f_cleanup_routing); $(declare -f f_add_routing); f_add_routing"
+sudo -S -E bash -c "$(declare -f f_cleanup_routing); $(declare -f f_add_routing); f_add_routing"
